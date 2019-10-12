b921f43fcfab4831071205687fe774126cc65e60
Sudi <vedarthlimited@gmail.com>
2019-10-12 10:49:59 +0100

    Initial Commit

---
+++ MyWeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyWeatherApp
+//
+//  Created by Vedarth Solutions on 10/12/19.
+//  Copyright © 2019 Vedarth Solutions. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
